d5efef6ef7dd4e9f7b737573a81ae36198c90774
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-11-25 22:17:32 +0530

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+const {
+  errorHandler,
+  notFoundHandler,
+  logHandler,
+  asyncHandler,
+  corsHandler,
+} = require("exhandlers");
+const express = require("express");
+
+const port = process.env.PORT;
+const hostname = process.env.HOSTNAME;
+const origins = process.env.ORIGINS;
+
+const server = express();
+
+server.use(express.json());
+server.use(corsHandler(origins));
+server.use(logHandler());
+
+server.get(
+  "/api/check",
+  asyncHandler(async (req, res) => {
+    res.status(200).json({
+      success: true,
+      message: "server online",
+    });
+  }),
+);
+
+server.use(notFoundHandler);
+server.use(errorHandler);
+
+server.listen(port, hostname, () => {
+  console.log(`server running @ http://${hostname}:${port}`);
+});
